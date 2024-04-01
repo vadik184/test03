1a3db7dcc8d513cd799030cd874435cb696158c6
vadik184 <133372327+vadik184@users.noreply.github.com>
2024-04-01 22:29:19 +0300

    й

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,29 +1,9 @@
-import React, { useEffect, useState } from 'react';
-import { FilmList } from 'components/FilmList/FilmList';
-import { getTrending } from 'components/API/API';
-// import { StyledApp } from 'components/AppStyle';
-// import { LoadMoreBtn } from 'components/Button/Button';
-// import { findImages } from 'components/Api/pixabay';
+import React from 'react';
 
 export const App = () => {
-  const [films, setFilms] = useState([]);
-  // const [isloading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    const fetchFilms = async () => {
-      try {
-        const trendingFilms = await getTrending();
-        setFilms(trendingFilms.results);
-      } catch (error) {
-        console.error(error.massage);
-      }
-    };
-    fetchFilms();
-  }, []);
   return (
     <>
       <h1>Trending today</h1>
-      <FilmList films={films} />
     </>
   );
 };
--- src/components/SearchBar/SearchForm.jsx
+++
@@ -1,6 +0,0 @@
-import React, { useState, useEffect } from 'react';
-import { searchFilm } from 'components/API/API';
-
-export const FilmSearching = () => {
-    const[]
-}
